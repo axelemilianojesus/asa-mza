cf52f39fb0d8cd080419ec0fdd129b30ec984c5c
Javier Pineda <javierpineda85@gmail.com>
2019-09-04 21:11:00 -0300

    Modificacion de Controllers
    Cambios en muni y tramite

--- app/Http/Controllers/MuniController.php
+++ app/Http/Controllers/MuniController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Muni;
 
 class MuniController extends Controller
 {
-    //
+    public function all(){
+      $munis =  Muni::paginate(20);
+      return view('municipalidades', compact('munis'));
+    }
+
+
 }
