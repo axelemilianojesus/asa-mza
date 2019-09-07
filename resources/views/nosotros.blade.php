d63c20a4de446cab34a1b6c633024cb14a06ce84
Nicolas <ncastro904@gmail.com>
2019-09-06 23:46:52 -0300

    modificacion
    nosotros y el css

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -24,19 +24,19 @@ Nosotros
             <p>La Asociación Solidaria de Arquitectos se está gestando como una Asociación Civil sin fines de lucro, en donde los profesionales puedan compartir de forma genuina, la búsqueda de objetivos comunes en un espacio colaborativo y cooperativo.</p>
         </section>
         <section class="mision">
-            <h2 id="naranja">● MISIÓN ●</h2>
+            <h2 id="naranja"> MISIÓN </h2>
             <p>Promover la participación de los miembros, para resolver las distintas problemáticas del rubro y brindar capacitaciones para el desarrollo profesional</p>
         </section>
         <section class="vision">
-            <h2 id="naranja">● VISIÓN ●</h2>
+            <h2 id="naranja"> VISIÓN </h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </section>
         <section class="enfoque">
-            <h2 id="naranja">●NUESTRO ENFOQUE●</h2>
+            <h2 id="naranja">NUESTRO ENFOQUE</h2>
             <p>Ser un espacio profesional, representativo de encuentro e interrelación que busca conciliar y lograr objetivos comunes</p>
         </section>
         <section class="comite">
-            <h2 id="naranja">● COMITÉ ●</h2>
+            <h2 id="naranja"> COMITÉ </h2>
             <article class="persona1">
                 <img class="main-photo" src="img/avatar/avatar1.jpg" alt="">
                 <p><b>Nombre:</b> lorem impsu</p>
