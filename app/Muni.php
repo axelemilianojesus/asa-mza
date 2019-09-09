914b89f7f3d55f7c8f14b543ac6739bf6fd33085
Javier Pineda <javierpineda85@gmail.com>
2019-09-09 19:46:58 -0300

    Modificacion de modelos

--- app/Muni.php
+++ app/Muni.php
@@ -1,13 +1,9 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
 class Muni extends Model
 {
     public $guarded=[];
-
     public function tramites() {
       return $this->hasMany("App\Tramite", "muni_id");
     }
