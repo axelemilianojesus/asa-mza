5728950c473d0127985d29e6d31c9753e270d347
Javier Pineda <javierpineda85@gmail.com>
2019-09-09 19:41:06 -0300

    Modificacion de Controladores

--- app/Http/Controllers/MuniController.php
+++ app/Http/Controllers/MuniController.php
@@ -1,22 +1,16 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Muni;
 use App\Tramite;
-
 class MuniController extends Controller
 {
     public function all(){
-
       $munis =  Muni::paginate(20);
       $vac=compact("munis");
       return view('municipalidades',$vac);
     }
-
     public function detail($id){
-
       $muni = Muni::find($id);
       $tramites = Tramite::where('munis_id','=',$id)->get();
       $vac=compact("muni","tramites");
--- app/Http/Controllers/TramiteController.php
+++ app/Http/Controllers/TramiteController.php
@@ -1,33 +1,26 @@
 <?php
-
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Tramite;
 use App\Muni;
-
 class TramiteController extends Controller
 {
     public function all(){
       $tramites =  Tramite::paginate(20);
       // return view('tramites/{id}', compact('tramites'));
-      return view('tramites',compact('tramites'));
+      // return view('tramites',compact('tramites'));
+      return view('tramites/{id}', compact('tramites'));
     }
-
     public function online(){
       return view('tramites-online'); // utilizado solo para presentar link externos
     }
-
     public function find($id){
       $tramites = Tramite::find($id);
-
       return view('tramites',compact('tramites'));
     }
     public function agregar(){
       $munis = Muni::all();
       $vac = compact("munis");
       return view('agregar-tramite',$vac);
-
     }
 }
