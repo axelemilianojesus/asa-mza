3e6f9e61f22aaf53f8447212486fea598edd8912
axelemiliano <axelemilianojesus@gmail.com>
2019-09-11 19:14:55 -0300

    Preguntas frecuentes
    Estilos

--- resources/views/preguntasfrecuentes.blade.php
+++ resources/views/preguntasfrecuentes.blade.php
@@ -11,6 +11,9 @@ Preguntas Frecuentes
 
 
 @section('main')
+  <div class="banner-standard bg-faq">
+      <h2 class="banner-texto-standard"><span>PREGUNTAS FRECUENTES</span></h2>
+  </div>
 <div class="container" id="preFrecuentes">
     <section class="preguntasFrecuentes">
         <div class="accordion" id="accordionExample">
@@ -20,13 +23,20 @@ Preguntas Frecuentes
 
                     <h2 class="mb-0">
                         <button class="btn btn-link m-auto" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            <h2>¿Pregunta Nº 1?</h2>
+                            <h2>¿De qué se trata?</h2>
                         </button>
                         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
                             <div class="-body">
-                                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                                    anim id est laborum. </p>
+                                <p> <strong>DIBUJOS</strong> para propiciar el trabajo de abstracción de elementos<br>
+
+                                    <strong>PROYECTOS</strong> para provocar la capacidad de síntesis y abstracción sobre diferentes aplicaciones y necesidades de comunicación visual.<br>
+
+                                    <strong>INSTALACIONES</strong> analizar y proponer y resolver los diversos sistemas de instalaciones para las edificaciones.<br>
+
+                                    <strong>CONSTRUCCIÓN</strong> Determinar en un proyecto Arquitectónico los principios estructurales de instalaciones, estimando su factibilidad y expresándolas en un proyecto constructivo, de acuerdo con las normas
+                                    pertinentes a las características particulares de cada proyecto.<br>
+
+                                    <strong>SOFTWARE</strong> que otorgan las herramientas necesarias para que el alumno resuelva técnicamente los proyectos.</p>
                             </div>
                         </div>
 
@@ -39,6 +49,9 @@ Preguntas Frecuentes
     <br>
 
     <section class="preguntasFrecuentes">
+
+
+
         <div class="accordion" id="accordionExample">
             <div class="tarjeta">
                 <div class="" id="headingTwo">
@@ -46,13 +59,18 @@ Preguntas Frecuentes
 
                     <h2 class="mb-0">
                         <button class="btn btn-link collapsed m-auto" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                            <h2>¿Pregunta Nº 2?</h2>
+                            <h2>¿En qué consiste el sistema de Créditos?</h2>
                         </button>
                         <div id="collapseTwo" class="collapse" aria-labelledby="collapseTwo" data-parent="#accordionExample">
                             <div class="-body">
-                                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                                    anim id est laborum. </p>
+                                <p> El sistema de créditos resulta como propuesta frente a la rigidez académica de los planes de estudio en los que los estudiantes toman las mismas asignaturas, con igual calendario y en una secuencia idéntica. Este
+                                    sistema se utiliza en los planes de estudio de currícula flexible, en los cuales cada alumno tiene oportunidad de seleccionar sus asignaturas aún cuando éstas se proporcionen en departamentos o carreras diferentes.
+                                    Permite llevar el registro del avance del estudiante a través de su plan de estudio individual y contar con criterios para la certificación de sus estudios superiores. El sistema de créditos permite asignar valores
+                                    iguales a cursos diferentes pero con ciertas características similares para contar con elementos para establecer equivalencias entre estos. La unidad utilizada en este sistema fue la hora-crédito, la cual se diseñó
+                                    con el objeto de proporcionar "una medida uniforme de la cantidad de tiempo dedicada a un propósito determinado...". El crédito es la unidad de valor o puntuación de una asignatura. Se calcula en la siguiente
+                                    forma:
+
+                                </p>
                             </div>
                         </div>
 
@@ -72,13 +90,11 @@ Preguntas Frecuentes
 
                     <h2 class="mb-0">
                         <button class="btn btn-link collapsed m-auto" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
-                            <h2>¿Pregunta Nº 3?</h2>
+                            <h2>¿Con qué recursos cuenta la licenciatura?</h2>
                         </button>
                         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
                             <div class="-body">
-                                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                                    anim id est laborum. </p>
+                                <p> La licenciatura cuenta con áreas específicas de estudio y de prácticas escolares. Cuenta con aulas con proyector y pantallas, taller de dibujo, taller de pincel de aire, laboratorios de mecánica de materiales, laboratorio de computación con equipo y software especializado en Arquitectura, cuenta también con aulas equipadas con restiradores para la práctica diaria de clases y con recursos bibliográficos exclusivos de Arquitectura y otras áreas afines.</p>
                             </div>
                         </div>
 
@@ -98,16 +114,13 @@ Preguntas Frecuentes
 
                     <h2 class="mb-0">
                         <button class="btn btn-link collapsed m-auto" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
-                            <h2>¿Pregunta Nº 4?</h2>
+                            <h2>¿En qué se distinguen los cursos de ASA?</h2>
                         </button>
                         <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
                             <div class="-body">
-                                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                                    anim id est laborum. </p>
+                                <p> La Universidad Iberoamericana contempla dentro de su currícula el Área de Reflexión Universitaria que favorece el encuentro del alumno consigo mismo, en su irrepetible soledad interior, para que afronte conscientemente su libertad y asuma responsablemente su propio desarrollo personal. Esta área consta de varios cursos en los que se aprenden conocimientos que, mediante una metodología adecuada, provocan el planteamiento de los cuestionamientos valorales más importantes que todo hombre debe hacerse a sí mismo, para clarificar e integrar sus valores y compromisos particulares con la sociedad.</p>
                             </div>
                         </div>
-
 
 
                 </div>
@@ -124,13 +137,13 @@ Preguntas Frecuentes
 
                     <h2 class="mb-0">
                         <button class="btn btn-link collapsed m-auto" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
-                            <h2>¿Pregunta Nº 5?</h2>
+                            <h2>¿Qué diseño se ajusta mejor a nuestras necesidades?</h2>
                         </button>
                         <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
                             <div class="-body">
-                                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                                    anim id est laborum. </p>
+                                <p> Dependiendo de la ciudad, área, barrio o zona, habrá diferentes ordenanzas que deben ser tomadas en cuenta y obedecidas, so pena de meternos en un montón de problemas y gastos innecesarios.
+
+Nuestro arquitecto podrá explicarnos cuáles son aquellas reglas que determinarán de una u otra forma detalles y variables del proyecto. Se trata de temas en general fastidiosos, pero tomemos en cuenta que son para beneficio nuestro y de todos los vecinos, así que lo mejor es tomárnoslo deportivamente. </p>
                             </div>
                         </div>
 
@@ -141,4 +154,4 @@ Preguntas Frecuentes
         </div>
     </section>
 
-@endsection
+    @endsection
