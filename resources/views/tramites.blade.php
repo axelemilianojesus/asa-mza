6b58a2cceac5898bd1ed43d8e34a5cb8c7a5b2e8
Javier Pineda <javierpineda85@gmail.com>
2019-09-10 21:46:10 -0300

    actualizacion de archivos

---
+++ resources/views/tramites.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.plantilla')
+@section('css')
+<link rel="stylesheet" href="../../css/styleTramitesMuni.css">
+@endsection
+@section('title')
+Tramites de {{$munis->name}}
+@endsection
+@section('main')
+ <div class="muni">
+   <div class="banner-standard bg-muni mb-0">
+       <h2 class="banner-texto-standard"><span>TRAMITES {{$munis->name}}</span></h2>
+   </div>
+     <div class="secciones max-width bg-image py-5">
+       {{-- <section class="seccionesmuni"> --}}
+         @foreach ($tramites as $tramite)
+           <ul>
+             <section class="seccionesmuni">
+             <li>
+               <p><a href="/tramites/{{$tramite->id}}">{{$tramite->title}}</a></p>
+               <p>aca descarga el archivo</p>
+               {{-- @if (Auth::user()->level === 1) --}}
+                 <button type="button" class="btn btn-secondary">Modificar</button>
+                 <button type="button" class="btn btn-danger">Eliminar</button>
+                 <button type="button" class="btn btn-secondary">Agregar tramite</button>
+               {{-- @endif --}}
+             </li>
+           </ul>
+           @endforeach
+         </section>
+     </div>
+ </div>
+@endsection
