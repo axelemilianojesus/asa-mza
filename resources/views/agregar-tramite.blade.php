f9900bdee160e1568efbef0eefbb434544d2c996
nicobende <niko_bendele@hotmail.com>
2019-09-11 21:43:37 -0300

    Agregar Tramite
    agregar tramite

--- resources/views/agregar-tramite.blade.php
+++ resources/views/agregar-tramite.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.plantilla')
 @section('css')
- <link rel="stylesheet" href="../../css/stylesMuni.css">
-
+ {{-- <link rel="stylesheet" href="../../css/styleTramites.css"> --}}
+ <link rel="stylesheet" href="../../css/stylecontacto.css">
 @endsection
 
 
@@ -11,12 +11,15 @@ Agregar trámites
 
 
 @section('main')
-  <div class="muni">
+  <div class="contenedorcontacto">
 
-    <div class="formLog" id="title">
-        <p class="info">Titulo del archivo</p>
-
-        <input id="title" type="text" class=" @error('title') is-invalid @enderror"  name="title" placeholder="Nombre " value="{{ old('title') }}" autofocus required>
+    <form action="" class="form">
+      <div class="form-header">
+          <h1 class="form-titulo"><span>Agregar trámite</span></h1>
+      </div>
+      <div>
+        <label for="nombre" class="form-label">Nombre del archivo:</label>
+        <input id="title" type="text" class="form-input @error('title') is-invalid @enderror"  name="title" placeholder="Nombre " value="{{ old('title') }}" autofocus required>
         @error('title')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -25,10 +28,12 @@ Agregar trámites
     </div>
 
     <div class="formLog" id="title">
-        <p class="info">Agregale una descipción</p>
 
-        <input id="description" type="textarea" class=" @error('description') is-invalid @enderror"  name="description" placeholder="Tu descripción va aqui " value="{{ old('description') }}" autofocus required>
-        @error('description')
+        <label for="nombre" class="form-label">Agregar descripción:</label>
+
+        <textarea id="description" class="form-textarea @error('description') is-invalid @enderror"  name="description" placeholder="Tu descripción va aqui " value="{{ old('description') }}" autofocus required></textarea>
+           @error ('description')
+
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
@@ -53,7 +58,6 @@ Agregar trámites
     </div>
     <div class="formLog" id="file">
         <p class="info">Seleccioná el archivo que vas a subir</p>
-
         <input id="file" class="file @error('file') is-invalid @enderror" type="file" name="avatar" value= "{{ old('file') }}">
         @error('avatar')
             <span class="invalid-feedback" role="alert">
@@ -61,6 +65,9 @@ Agregar trámites
             </span>
         @enderror
     </div>
+    <br>
+    <input type="submit" class="btn-submit" value="Guardar">
+    </form>
 
   </div>
 @endsection
