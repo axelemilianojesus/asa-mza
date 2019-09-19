1a64b934343a4a3e1882ab39d3aae2a227562dca
nicobende <niko_bendele@hotmail.com>
2019-09-18 22:01:05 -0300

    Header
    Color letra menu mi perfil

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -128,7 +128,7 @@
                       </a>
                       <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                           <a class="dropdown-item" href="/perfil"style= color:white>Mi perfil</a>
-                          <a class="dropdown-item" style= color:white href="{{ route('logout') }}" onclick="event.preventDefault();
+                          <a class="dropdown-item"  href="{{ route('logout') }}" onclick="event.preventDefault();
                                                    document.getElementById('logout-form').submit();">
                               {{ __('Cerrar Sesión') }}
                           </a>
