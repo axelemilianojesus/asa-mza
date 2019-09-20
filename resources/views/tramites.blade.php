0cea8966d2dc9c16b7663a4881c0eb3d0863dc2d
Javier Pineda <javierpineda85@gmail.com>
2019-09-20 12:15:30 -0300

    Correccion de detalles de CSS en tramites

--- resources/views/tramites.blade.php
+++ resources/views/tramites.blade.php
@@ -9,48 +9,50 @@ Tramites de {{$munis->name}}
 @endsection
 
 @section('js')
-  <script type="text/javascript" src="../../js/tramites.js">
+<script type="text/javascript" src="../../js/tramites.js">
 
-  </script>
+</script>
 @endsection
 
 @section('main')
- <div class="muni">
+<div class="muni">
 
-   <div class="banner-standard bg-muni mb-0">
-       <h2 class="banner-texto-standard titulos-naranja"><span>TRAMITES {{$munis->name}}</span></h2>
-   </div>
-     <div class="secciones max-width bg-image py-5">
-       {{-- <section class="seccionesmuni"> --}}
-         @foreach ($tramites as $tramite)
-           <ul>
-             <section class="seccionesmuni">
-             <li>
-               {{-- <p><a href="/tramites/{{$tramite->id}}">{{$tramite->title}}</a></p> --}}
+    <div class="banner-standard bg-muni mb-0">
+        <h2 class="banner-texto-standard titulos-naranja"><span>TRAMITES {{$munis->name}}</span></h2>
+    </div>
+    <div class="secciones max-width bg-image py-5">
+        {{-- <section class="seccionesmuni"> --}}
+        @foreach ($tramites as $tramite)
+        <ul>
+            <section class="seccionesmuni">
+                <li>
+                    {{-- <p><a href="/tramites/{{$tramite->id}}">{{$tramite->title}}</a></p> --}}
 
-               <p>
-                 <a href="../storage/app/public/{{$tramite->file}}" download="{{$tramite->title}}">
-                     {{$tramite->title}}
-                 </a>
-               </p>
-               <p>{{$tramite->description}}</p>
-               @if (Auth::check())
+                    <p>
+                        <a href="../storage/app/public/{{$tramite->file}}" download="{{$tramite->title}}">
+                            {{$tramite->title}}
+                        </a>
+                      
+                    </p>
+                    <p>{{$tramite->description}}</p>
 
-                 <button type="button" class="btn btn-secondary">Modificar</button>
-                 <form class="" action="/tramites/eliminar" method="post">
-                   @csrf
-                   <input type="hidden" name="id" value="{{$tramite->id}}">
-                    <button type="submit" class="btn btn-danger"onclick="deleletconfig()">Eliminar</button>
-                 </form>
+                    @if (Auth::check())
+                    <div class="checked">
 
 
-                 {{-- <button type="button" class="btn btn-secondary">
-                 <a href="/agregar-tramite">Agregar tramite</a></button> --}}
-               @endif
-             </li>
-           </ul>
-           @endforeach
-         </section>
-     </div>
- </div>
+                        <button type="button" class="btn btn-secondary">Modificar</button>
+                        <form class="" action="/tramites/eliminar" method="post">
+                            @csrf
+                            <input type="hidden" name="id" value="{{$tramite->id}}">
+                            <button type="submit" class="btn btn-danger" onclick="deleletconfig()">Eliminar</button>
+                        </form>
+
+                    </div>
+                    @endif
+                </li>
+        </ul>
+        @endforeach
+        </section>
+    </div>
+</div>
 @endsection
